f2637eec5d8e9d4e45218dbc128fdf6b27f1478c
TCAGM-Dev <thomas@andringa.org>
2025-03-20 11:01:42 +0100

    Add "success" boolean to status code for easier success checking

---
+++ src/test/java/io/github/tcagmdev/requester/test/TestValues.java
@@ -0,0 +1,5 @@
+package io.github.tcagmdev.requester.test;
+
+public abstract class TestValues {
+	public static final String ALL_CHARS = " \t\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW0123456789`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?¡²³¤€¼½¾‘’¥×«»¬¶´ç¿¹£÷¦°¨Çäåé®þüúíóöáðæ©ñµÄÅÉÞÜÚÍÓÖÁ§ÐØÆ¢Ñ";
+}
